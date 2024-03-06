4b200a4ff989b126138ee725c6e10c5fed71c468
sfelsch <felsch29@gmail.com>
2024-03-06 15:12:55 -0300

    Filtro agregado para SAI

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import InputText from './components/InputText';
 import Button from './components/Button';
 import Grid from './components/Grid';
-import Spinner from './components/Spinner'; // Importa tu componente Spinner aquí
+import Spinner from './components/Spinner';
 import './App.css';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
-  const [loading, setLoading] = useState(false); // Nuevo estado para el spinner
+  const [loading, setLoading] = useState(false);
 
   const handleSearch = async () => {
-    setLoading(true); // Mostrar spinner al iniciar la búsqueda
+    setLoading(true);
     try {
       const encodedSearchQuery = encodeURIComponent(searchQuery);
       const response = await fetch(`https://snowstorm-test.msal.gob.ar/MAIN/concepts?term=${encodedSearchQuery}&offset=0&limit=50`, {
@@ -25,14 +25,19 @@ function App() {
       }
       const data = await response.json();
       if (data.items) {
-
-        const filteredData = data.items.filter(item => item.fsn.term.includes('(trastorno)'));
+        const filteredData = data.items.filter(item => {
+          // filtro los q tienen trastorno
+          const hasTrastorno = item.fsn.term.includes('(trastorno)');
+          // saco los SAI
+          const noSAI = !item.fsn.term.includes('SAI');
+          return hasTrastorno && noSAI;
+        });
         const formattedData = filteredData.map(item => ({
           conceptId: item.conceptId,
           term: item.fsn.term
         }));
         setSearchResults(formattedData);
-      } else {
+      }else {
         throw new Error('La respuesta de la API no contiene la propiedad "items"');
       }
     } catch (error) {
@@ -60,7 +65,7 @@ function App() {
       </div>
     
     <Grid data={searchResults} />
-    {loading && <Spinner />} {/* Mostrar el Spinner solo si loading es true */}
+    {loading && <Spinner />}
   </div>
   );
 }
