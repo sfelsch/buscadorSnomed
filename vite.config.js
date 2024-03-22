5d2c59b15d851dd13cb06707008ff6c260035e5c
sfelsch <felsch29@gmail.com>
2024-03-22 09:49:02 -0300

    docker funcionando?

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  preview: {
+    port: 9090,
+    strictPort: true,
+   },
   server: {
     proxy: {
       '/snowstorm': {
@@ -12,6 +16,10 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/snowstorm/, ''),
       },
     },
-    port: 9090, // Cambiar el puerto a 9090
+    port: 9090,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:9090",
   },
 });
+
